d7a4c6cd9c5c3d53b5f20e837f8810683a076e33
Frankie Murillo <frankidatank@gmail.com>
2021-04-06 14:35:22 -0700

    converted to async

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-const submitBtn = document.getElementById('submit');
-const inputValue = document.getElementById('search');
-const apiKey = '58ccf8033c6a3809bd3a8bd65bc7b147';
-const degress = document.getElementById('degress');
+const submitBtn = document.getElementById("submit");
+const inputValue = document.getElementById("search");
+const apiKey = "58ccf8033c6a3809bd3a8bd65bc7b147";
+const degress = document.getElementById("degress");
 
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-submitBtn.addEventListener('click', () => {
-fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&appid=' + apiKey).then(
-    response => response.json()).then(data => {
-        console.log(data);
-        let nameValue = data['name'];
-        let tempValue = data['main']['temp'];
-        let fconvert = Math.floor(((tempValue - 273.15) * 9/5) + 32);
-        let descValue = data['weather'][0]['description'];
-        let answer;
-        fconvert < 70 ? answer = "Better grab that 🧥!" : answer = "No need for a 🧥";
-        degress.innerHTML = `Looks like it is gonna be ${fconvert} degress Farenheight! \n ${answer}`;
-
-    }).catch(err => {
-        alert("Wrong City Value, try agian");
-        console.log(err);
-})
-
-
+submitBtn.addEventListener("click", async () => {
+  const data = await getWeatherData(inputValue, apiKey);
+  let tempValue = data["main"]["temp"];
+  let fconvert = Math.floor(((tempValue - 273.15) * 9) / 5 + 32);
+  let answer;
+  fconvert < 70
+    ? (answer = "Better grab that 🧥!")
+    : (answer = "No need for a 🧥");
+  degress.innerHTML = `Looks like it is gonna be ${fconvert} degress Farenheight! \n ${answer}`;
 });
+
+async function getWeatherData(inputValue, apiKey) {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value} 
+    &appid=${apiKey}
+`);
+  if (!response) {
+    alert("Error, try again");
+  }
+  const data = await response.json();
+  return data;
+}
