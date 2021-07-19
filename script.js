15ba13481a74fcf6e04643880414a192b494ce02
Frankie Murillo <frankidatank@gmail.com>
2021-07-19 09:13:23 -0700

    title change

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const submitBtn = document.getElementById("submit");
 const inputValue = document.getElementById("search");
 const apiKey = "58ccf8033c6a3809bd3a8bd65bc7b147";
-const degress = document.getElementById("degress");
+const degrees = document.getElementById("degrees");
 
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 submitBtn.addEventListener("click", async () => {
   const data = await getWeatherData(inputValue, apiKey);
   let tempValue = data["main"]["temp"];
@@ -12,11 +11,12 @@ submitBtn.addEventListener("click", async () => {
   fconvert < 65
     ? (answer = "Better grab that 🧥!")
     : (answer = "No need for a 🧥");
-  degress.innerHTML = `Looks like it is gonna be ${fconvert} degress Farenheight in ${inputValue.value}! \n ${answer}`;
+  degrees.innerHTML = `Looks like it is gonna be ${fconvert} degrees Farenheight in ${inputValue.value}! \n ${answer}`;
 });
 
 async function getWeatherData(inputValue, apiKey) {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value} 
+  const response =
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value} 
     &appid=${apiKey}
 `);
   const data = await response.json();
